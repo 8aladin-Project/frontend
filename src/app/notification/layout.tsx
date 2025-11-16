724b1b9d16950d427a0d5b816caf9481e5e9e2b5
kdyun <kdyun9288@gmail.com>
2025-11-16 18:59:43 +0900

    design: 알림 페이지 컴포넌트 교체

---
+++ src/app/notification/layout.tsx
@@ -0,0 +1,13 @@
+import ClientLayout from "@/components/layout/ClientLayout";
+
+export default function NotificationLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClientLayout title="알림" showHeader={true} showBackButton={true}>
+      {children}
+    </ClientLayout>
+  );
+}
--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -1,12 +1,10 @@
 import NotificationPage from "./NotificationPage";
-import Header from "./Header";
 import Footer from "@/components/layout/Footer";
 
 
 export default function Page() {
   return(
     <div>
-      <Header/>
       <NotificationPage/>
       <Footer/>
     </div>
